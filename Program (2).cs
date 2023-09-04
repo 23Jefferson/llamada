62d41498609c9aaa5112e51b5353772bfbaf7c82
Jefferson Eduardo Cornejo Guerrero <142254794+23Jefferson@users.noreply.github.com>
2023-09-03 19:24:32 -0600

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definición de la tabla de precios por zona
+        int[] claves = { 12, 15, 18, 19, 23 };
+        string[] zonas = { "América del norte", "América central", "América del sur", "Europa", "Asia" };
+        double[] precios = { 2.0, 2.2, 4.5, 3.5, 6.0 };
+
+        // Pedir la clave de zona al usuario
+        Console.Write("Ingrese la clave de la zona (12, 15, 18, 19 o 23): ");
+        if (!int.TryParse(Console.ReadLine(), out int claveZona) || Array.IndexOf(claves, claveZona) == -1)
+        {
+            Console.WriteLine("Clave de zona no válida. Por favor, ingrese una clave válida.");
+            return;
+        }
+
+        // Pedir el número de minutos hablados al usuario
+        Console.Write("Ingrese el número de minutos hablados: ");
+        if (!double.TryParse(Console.ReadLine(), out double minutosHablados))
+        {
+            Console.WriteLine("Número de minutos no válido. Por favor, ingrese un número válido.");
+            return;
+        }
+
+        // Encontrar el índice correspondiente a la clave de zona
+        int indice = Array.IndexOf(claves, claveZona);
+
+        // Calcular el costo de la llamada
+        double costo = precios[indice] * minutosHablados;
+        Console.WriteLine($"El costo de la llamada a {zonas[indice]} es: ${costo}");
+    }
+}
